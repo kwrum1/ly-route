b59c0a88b447812060abd99b388bd63ea88290d1
kwrum1 <kwrum1@users.noreply.github.com>
2026-08-04 12:55:57 +0800

    ci: disable obsolete live-build security mirror

--- scripts/build-auto-install-iso.sh
+++ scripts/build-auto-install-iso.sh
@@ -178,6 +178,7 @@ else
     cd "$work"
     lb config --mode debian --distribution "$suite" --architectures amd64 \
       --binary-images iso-hybrid --archive-areas main --apt-recommends false \
+      --security false \
       --bootappend-live 'boot=live components quiet lyroute.autoinstall=1' \
       --iso-volume 'LY_ROUTE_INSTALL' --memtest none
     lb build
